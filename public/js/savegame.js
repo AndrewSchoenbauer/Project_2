e5caeef95aceb75596ae38695c5472c90c85a812
Andrew <andrewschoenbauer12@gmail.com>
2022-02-06 17:45:52 -0600

    messed with the saved games

--- public/js/savegame.js
+++ public/js/savegame.js
@@ -1,8 +1,10 @@
-// const savedGamesList = []
+
+const savedGamesList = JSON.parse(localStorage.getItem('savedgamelist')) || [];
+const savedGameEl = document.getElementById("saved")
 const saveGameHandler =  (event) => {
     event.preventDefault();
 
-const savedGamesList = JSON.parse(localStorage.getItem('savedgamelist')) || [];
+
 
     const gameId = event.target.dataset.gameid;
     const playerId = event.target.dataset.playerid;
@@ -41,5 +43,33 @@ const savedGamesList = JSON.parse(localStorage.getItem('savedgamelist')) || [];
   localStorage.setItem('savedgamelist', JSON.stringify(savedGamesList))
 
 }
+const renderSavedGames = () => {
+    savedGameEl.textContent = "";
+    for (let i=0; i<savedGamesList.length; i++){
+        for (let i=0; i<savedGamesList[i].savedgames.length; i++) {
+            const cityHistory = document.createElement("input");
+            cityHistory.setAttribute("type","text");
+            cityHistory.setAttribute("readonly",true);
+            cityHistory.setAttribute("class", "form-control d-block font-weight-bold border border-dark rounded bg-$cyan-200 mt-2");
+            cityHistory.setAttribute("value", savedGamesList[i].savedgames);
+            // cityHistory.addEventListener("click",function() {
+            //     getWeather(cityHistory.value);
+            //     getFivedayWeather(cityHistory.value);
+            //     console.log(cityHistory.value);
+            // })
+            console.log(cityHistory);
+            savedGameEl.append(cityHistory);
+        }
+
+    }
+     }
+     renderSavedGames();
+    //  if (searchHistory.length > 0) {
+    //      getWeather(searchHistory[searchHistory.length - 1])
+    //      getFivedayWeather(searchHistory[searchHistory.length - 1])
+    //  }
+    
+
+ 
 
 document.querySelector('#save-btn').addEventListener('click', saveGameHandler)
