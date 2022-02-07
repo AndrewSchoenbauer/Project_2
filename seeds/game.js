8f34b4c72f3450e92cdce62590e03f0c0e58b841
Andrew <andrewschoenbauer12@gmail.com>
2022-02-07 15:31:11 -0600

    changed color

--- seeds/game.js
+++ seeds/game.js
@@ -60,6 +60,66 @@ const gameData =
         image: "/images/warzone.jpg",
         developer: "Activision",
         release_day: 2020
+    },
+    {
+        name: "Dota 2",
+        image: "/images/dota-2.jpg",
+        developer: "Valve",
+        release_day: 2013
+    },
+    {
+        name: "Halo: Infinity",
+        image: "/images/halo.jpg",
+        developer: "343 Industries",
+        release_day: 2021
+    },
+    {
+        name: "Rocket League",
+        image: "/images/rocket-league.jpg",
+        developer: "Psyonix",
+        release_day: 2015
+    },
+    {
+        name: "Grand Theft Auto V",
+        image: "/images/gta-5.png",
+        developer: "Rockstar Games",
+        release_day: 2013
+    },
+    {
+        name: "Overwatch",
+        image: "/images/overwatch.jpg",
+        developer: "Activision Blizzard",
+        release_day: 2016
+    },
+    {
+        name: "Dead by Daylight",
+        image: "/images/deadby.jpg",
+        developer: "Deep Silver",
+        release_day: 2016
+    },
+    {
+        name: "Rust",
+        image: "/images/rust.jpg",
+        developer: "Facepunch Studios",
+        release_day: 2018
+    },
+    {
+        name: "Red Dead Redemption 2",
+        image: "/images/reddead.jpg",
+        developer: "Rockstar Games",
+        release_day: 2018
+    },
+    {
+        name: "Runescape",
+        image: "/images/runescape.jpg",
+        developer: "Jagex",
+        release_day: 2001
+    },
+    {
+        name: "Tom Clancy's Rainbow Six Siege",
+        image: "/images/rainbow.jpg",
+        developer: "Ubisoft Montreal",
+        release_day: 2015
     }
 ]
 const seedGame =() => Game.bulkCreate(gameData);
